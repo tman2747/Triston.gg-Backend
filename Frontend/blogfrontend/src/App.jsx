a02287929873b7b78a338b71f37a9d69d772b1aa
Triston Land <tristonland@gmail.com>
2026-01-13 02:43:01 -0700

    bad frontend stuff

--- Frontend/blogfrontend/src/App.jsx
+++ Frontend/blogfrontend/src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [accessToken, setData] = useState({});
+  const [user, setUser] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
+  async function fetchLogin() {
+    const res = await fetch("http://127.0.0.1:3000/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username: username, password: password }),
+    });
+    if (!res.ok) {
+      console.error("Request failed :(", res.status);
+      return;
+    }
+    const result = await res.json();
+    setData(result);
+    console.log(result);
+  }
+  useEffect(() => {
+    async function getUser() {
+      if (!accessToken.accessToken) {
+        return;
+      }
+      const res = await fetch("http://127.0.0.1:3000/auth/me", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${accessToken.accessToken}`,
+        },
+      });
+      const result = await res.json();
+      setUser(result);
+      console.log(result);
+    }
+    getUser();
+  }, [accessToken]);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>hello {user?.name ?? "world"}</div>
+      <div>this is the new site</div>
+      <input
+        type="text"
+        placeholder="UserName"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={fetchLogin}>login</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
